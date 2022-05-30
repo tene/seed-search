8f5fea34d0434c2d5b9beaf62861d1e8c732bf08
Stephen Weeks <tene@allalone.org>
2022-05-30 16:33:15 -0700

    typo

--- scripts/run-scrape.sh
+++ scripts/run-scrape.sh
@@ -14,7 +14,7 @@ if [ ! -f "$cache" ] ; then
     TMP="$(mktemp $cache.XXXXXX.tmp)"
     cp -f scripts/scrape-seed.lua "$CRAWL_PATH"/crawl-ref/source/scripts/scrape-seed.lua 2>&1 >/dev/null
     if "$CRAWL_PATH"/crawl-ref/source/util/fake_pty "$CRAWL_PATH"/crawl-ref/source/crawl -script scrape-seed.lua "$SEED" 2>&1 | sed '/^$/d' | gzip > "$TMP" ; then
-        mv "$TMP" "cache"
+        mv "$TMP" "$cache"
     else
         mkdir -p cache/errors
         mv "$TMP" cache/errors
